e128c5dabe18f71f9f7e7dd7f92b676893b1950a
victor_xu <pure90719@gmail.com>
2025-05-25 16:12:02 +0800

    W14-P2: Show static products page

--- demo/w14_fullstack_43/server_43/server_43.js
+++ demo/w14_fullstack_43/server_43/server_43.js
@@ -5,9 +5,9 @@ const app_43 = express();
 app_43.use(express.static("public"));
 app_43.set("view engine", "ejs");
 
-app_43.use("/", (req, res, next) => {
-  res.render("index", {
-    title: "Express",
+app_43.use("/product_43/static", (req, res, next) => {
+  res.render("product_43/static", {
+    title: "Get Products - Static",
     name: "VICTOR HSU",
     id: "213410243",
   });
